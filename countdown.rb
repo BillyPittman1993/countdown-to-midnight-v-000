e606334e2ece9dd378635bcc14a74935bd315c7c
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-06-20 15:49:37 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,8 @@
- countdown_output = 3
+ countdown_output = 10
 
 def countdown(countdown_output)
   while countdown_output > 0
     puts "#{countdown_output} SECOND(S)!"
     countdown_output  -= 1 
   end
-    puts "HAPPY NEW YEAR!"
 end
